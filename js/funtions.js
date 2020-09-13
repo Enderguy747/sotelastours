7f3d1ad72f135aa9f4edfbbc78247ada9bba2b4b
Enderguy747 <jo.sed98@hotmail.com>
2020-09-12 23:55:53 -0600

    creacion del proyecto

---
+++ js/funtions.js
@@ -0,0 +1,15 @@
+"use strict";
+function toggleMenu() {
+  const menuToggle = document.querySelector(".toggle");
+  const navigation = document.querySelector(".navigation");
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
+function videoTogle(){
+    const trailer = document.querySelector(".trailer");
+    const video = document.querySelector("video");
+    trailer.classList.toggle("active");
+    video.currentTime = 0
+    video.pause()
+
+}
